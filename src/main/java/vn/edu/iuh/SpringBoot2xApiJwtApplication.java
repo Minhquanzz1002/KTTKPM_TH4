86cf91f35e9a50d7a6b1eedc90ae3fc63e15d1fa
Nguyễn Minh Quân <20098151.quan@student.iuh.edu.vn>
2023-07-05 01:56:46 +0700

    init project

---
+++ src/main/java/vn/edu/iuh/SpringBoot2xApiJwtApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2xApiJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2xApiJwtApplication.class, args);
+    }
+
+}
